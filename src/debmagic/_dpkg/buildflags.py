d90ab05c03a86237edd8d6d59485cccce5a410e8
Jonas Jelten <jj@sft.lol>
2025-11-30 23:15:00 +0100

    feat(package_version): store package version in a struct

--- src/debmagic/_dpkg/buildflags.py
+++ src/debmagic/_dpkg/buildflags.py
@@ -1,19 +1,17 @@
 import json
 import os
-import re
-import shlex
 import subprocess
 from pathlib import Path
 
-
-def _run_output(cmd: str, input: str | None = None, env: dict[str, str] | None = None, cwd: Path | None = None) -> str:
-    input_data = None
-    if input:
-        input_data = input.encode()
-    return subprocess.check_output(shlex.split(cmd), input=input_data, env=env).strip().decode()
+from .._package_version import PackageVersion
+from .._utils import run_cmd
 
 
-def get_flags(package_dir: Path, maint_options: str | None = None) -> dict[str, str]:
+def _cmd(cmd: str, input_data: str | None = None, env: dict[str, str] | None = None, cwd: Path | None = None) -> str:
+    return run_cmd(cmd, check=True, env=env, input=input_data, text=True, capture_output=True).stdout.strip()
+
+
+def get_pkg_env(package_dir: Path, maint_options: str | None = None) -> tuple[dict[str, str], PackageVersion]:
     """
     does what including "/usr/share/dpkg/buildflags.mk" would do.
     """
@@ -24,7 +22,7 @@ def get_flags(package_dir: Path, maint_options: str | None = None) -> dict[str, 
         # TODO more vars as parameters, e.g. DEB_CFLAGS_MAINT_APPEND
 
     # get build flags
-    flags_raw = _run_output("dpkg-buildflags", env=result)
+    flags_raw = _cmd("dpkg-buildflags", env=result)
     for flag_line in flags_raw.splitlines():
         flag_name, _, flag_value = flag_line.partition("=")
         result[flag_name] = flag_value
@@ -37,29 +35,32 @@ def get_flags(package_dir: Path, maint_options: str | None = None) -> dict[str, 
 
     # architecture.mk
     if result.get("DEB_HOST_ARCH") is None:
-        arch_flags_raw = _run_output("dpkg-architecture", env=result)
+        arch_flags_raw = _cmd("dpkg-architecture", env=result)
         for arch_flag_line in arch_flags_raw.splitlines():
             arch_flag_name, _, arch_flag_value = arch_flag_line.partition("=")
             result[arch_flag_name] = arch_flag_value
 
     # pkg-info.mk
     if result.get("DEB_SOURCE") is None or result.get("DEB_VERSION") is None:
-        result["DEB_SOURCE"] = _run_output("dpkg-parsechangelog -SSource", env=result, cwd=package_dir)
-        result["DEB_VERSION"] = _run_output("dpkg-parsechangelog -SVersion", env=result, cwd=package_dir)
+        result["DEB_SOURCE"] = _cmd("dpkg-parsechangelog -SSource", env=result, cwd=package_dir)
+        result["DEB_VERSION"] = _cmd("dpkg-parsechangelog -SVersion", env=result, cwd=package_dir)
+        version = PackageVersion.from_str(result["DEB_VERSION"])
+
         # this would return DEB_VERSION in pkg-info.mk if no epoch is in version.
         # instead, we return "0" as oritinally intended if no epoch is in version.
-        result["DEB_VERSION_EPOCH"] = (
-            "0" if ":" not in result["DEB_VERSION"] else re.sub(r"^([0-9]+):.*$", r"\1", result["DEB_VERSION"])
-        )
-        result["DEB_VERSION_EPOCH_UPSTREAM"] = re.sub(r"^(.*?)(-.*)?$", r"\1", result["DEB_VERSION"])
-        result["DEB_VERSION_UPSTREAM_REVISION"] = re.sub(r"^([0-9]*:)?(.*?)$", r"\2", result["DEB_VERSION"])
-        result["DEB_VERSION_UPSTREAM"] = re.sub(r"^([0-9]*:)(.*?)", r"\2", result["DEB_VERSION_EPOCH_UPSTREAM"])
-        result["DEB_VERSION_REVISION"] = re.sub(r"^.*?-([^-]*)$", r"\1", result["DEB_VERSION"])
-        result["DEB_DISTRIBUTION"] = _run_output("dpkg-parsechangelog -SDistribution", env=result, cwd=package_dir)
-        result["DEB_TIMESTAMP"] = _run_output("dpkg-parsechangelog -STimestamp", env=result, cwd=package_dir)
+        result["DEB_VERSION_EPOCH"] = version.epoch
+        result["DEB_VERSION_EPOCH_UPSTREAM"] = version.epoch_upstream
+        result["DEB_VERSION_UPSTREAM_REVISION"] = version.upstream_revision
+        result["DEB_VERSION_UPSTREAM"] = version.upstream
+        result["DEB_VERSION_REVISION"] = version.revision
+
+        result["DEB_DISTRIBUTION"] = _cmd("dpkg-parsechangelog -SDistribution", env=result, cwd=package_dir)
+        result["DEB_TIMESTAMP"] = _cmd("dpkg-parsechangelog -STimestamp", env=result, cwd=package_dir)
 
         if result.get("SOURCE_DATE_EPOCH") is None:
             result["SOURCE_DATE_EPOCH"] = result["DEB_TIMESTAMP"]
+    else:
+        version = PackageVersion.from_str(result["DEB_VERSION"])
 
     if result.get("ELF_PACKAGE_METADATA") is None:
         elf_meta = {
@@ -74,4 +75,4 @@ def get_flags(package_dir: Path, maint_options: str | None = None) -> dict[str, 
 
         result["ELF_PACKAGE_METADATA"] = json.dumps(elf_meta, separators=(",", ":"))
 
-    return result
+    return result, version
